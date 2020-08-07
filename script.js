29503f548181a1f3ea23d0d2bb54e432683638aa
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-08-07 15:28:25 +0300

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+// const pageUp = document.querySelector('.scrol');
+
+// document.addEventListener('scroll', () => {
+
+//     if (document.documentElement.scrollTop > 800){
+//         pageUp.classList.add('block');
+//     } else {
+//         pageUp.classList.remove('block');
+//     }
+// });
+// console.log(document.body.offsetHeight);
+
+
