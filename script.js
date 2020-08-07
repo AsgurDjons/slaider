d16558ef0c947a0fce68215f799b3cd9bd5c8119
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-08-07 20:28:30 +0300

    prev next js slaider

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const slaiders = document.querySelectorAll('.slaider__iner'),
         items = document.querySelector('.slaider_items'),
         item = document.querySelectorAll('.slaider_item'),
+        nav = document.querySelector('.slaider_nav'),
         next = document.querySelector('.next'),
         prev = document.querySelector('.prev');
 
@@ -32,22 +33,27 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+    
+    let i = 0;
+    nav.addEventListener('click', (e) => {
+        hidenSlaider();
+        const target = e.target;
+        if (target && target.classList.contains('next')) {
+            hidenSlaider();
+            slaiderShow(i);
+            i--;
+            if (i < 0) {
+                i = 4;
+            }
+        } else {
+            if (target && target.classList.contains('prev')) {
+                hidenSlaider();
+                slaiderShow(i);
+                i++;
+                if (i > 4) {
+                    i = 0;
+                }
+            }
+        }
+    });
 });
-
-
-// next.onclick = function () {
-//     images[counter].style.display = "none";
-//     counter++;
-//     if (counter >= images.length) {
-//         counter = 0;
-//     }
-//     images[counter].style.display = "block";
-// };
-// prev.onclick = function () {
-//     images[counter].style.display = "none";
-//     counter--;
-//     if (counter < 0) {
-//         counter = 5;
-//     }
-//     images[counter].style.display = "block";
-// };
