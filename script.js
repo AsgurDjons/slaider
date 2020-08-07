861bad4cec9a1c4c25e7e589cb8119acc553ecd6
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-08-07 16:37:39 +0300

    slaider tabs

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 'use strict';
-// const pageUp = document.querySelector('.scrol');
+window.addEventListener('DOMContentLoaded', () => {
+    const slaiders = document.querySelectorAll('.slaider__iner'),
+    items = document.querySelector('.slaider_items'),
+    item = document.querySelectorAll('.slaider_item');
 
-// document.addEventListener('scroll', () => {
-
-//     if (document.documentElement.scrollTop > 800){
-//         pageUp.classList.add('block');
-//     } else {
-//         pageUp.classList.remove('block');
-//     }
-// });
-// console.log(document.body.offsetHeight);
+    function slaiderShow (i = 0) {
+        slaiders[i].classList.add('show');
+        item[i].classList.add('open');
+    }
+    function hidenSlaider () {
+        slaiders.forEach (item => {
+            item.classList.remove('show');
+        });
+        item.forEach (item => {
+            item.classList.remove('open');
+        });
+    }
+    hidenSlaider();
+    slaiderShow();
+    items.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target && target.classList.contains('slaider_item')) {
+            item.forEach ((item, i)=> {
+                if (item == target) {
+                    hidenSlaider(); 
+                    slaiderShow(i);
+                }
+            });
+        }
+    });
+});
 
 
