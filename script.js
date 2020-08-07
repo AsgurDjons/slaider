81ddf556a074f69dd60b54ee290ed932bb0ae079
AsgurDjons <61666677+AsgurDjons@users.noreply.github.com>
2020-08-07 17:32:32 +0300

    Style prev next slaider

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
     const slaiders = document.querySelectorAll('.slaider__iner'),
-    items = document.querySelector('.slaider_items'),
-    item = document.querySelectorAll('.slaider_item');
+        items = document.querySelector('.slaider_items'),
+        item = document.querySelectorAll('.slaider_item'),
+        next = document.querySelector('.next'),
+        prev = document.querySelector('.prev');
 
-    function slaiderShow (i = 0) {
+    function slaiderShow(i = 0) {
         slaiders[i].classList.add('show');
         item[i].classList.add('open');
     }
-    function hidenSlaider () {
-        slaiders.forEach (item => {
+
+    function hidenSlaider() {
+        slaiders.forEach(item => {
             item.classList.remove('show');
         });
-        item.forEach (item => {
+        item.forEach(item => {
             item.classList.remove('open');
         });
     }
@@ -21,9 +24,9 @@ window.addEventListener('DOMContentLoaded', () => {
     items.addEventListener('click', (e) => {
         const target = e.target;
         if (target && target.classList.contains('slaider_item')) {
-            item.forEach ((item, i)=> {
+            item.forEach((item, i) => {
                 if (item == target) {
-                    hidenSlaider(); 
+                    hidenSlaider();
                     slaiderShow(i);
                 }
             });
@@ -32,3 +35,19 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
+// next.onclick = function () {
+//     images[counter].style.display = "none";
+//     counter++;
+//     if (counter >= images.length) {
+//         counter = 0;
+//     }
+//     images[counter].style.display = "block";
+// };
+// prev.onclick = function () {
+//     images[counter].style.display = "none";
+//     counter--;
+//     if (counter < 0) {
+//         counter = 5;
+//     }
+//     images[counter].style.display = "block";
+// };
